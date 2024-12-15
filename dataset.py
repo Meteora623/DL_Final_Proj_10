d7bf59d9b11c9d4bbff1cdb38e484bea9a60f94d
Meteora623 <141472938+Meteora623@users.noreply.github.com>
2024-12-15 17:45:59 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 from typing import NamedTuple, Optional
 import torch
 import numpy as np
+import torchvision.transforms as transforms
 
 
 class WallSample(NamedTuple):
@@ -9,14 +10,10 @@ class WallSample(NamedTuple):
     actions: torch.Tensor
 
 
-class WallDataset(torch.utils.data.Dataset):
-    def __init__(
-        self,
-        data_path: str,
-        probing: bool = False,
-        device: str = "cuda",
-    ):
+class WallDataset:
+    def __init__(self, data_path, probing=False, device="cuda", augment=False):
         self.device = device
+        self.augment = augment
         self.states = np.load(f"{data_path}/states.npy", mmap_mode="r")
         self.actions = np.load(f"{data_path}/actions.npy")
 
@@ -25,32 +22,46 @@ class WallDataset(torch.utils.data.Dataset):
         else:
             self.locations = None
 
+        # Augmentation transforms
+        if self.augment:
+            self.augmentation_transforms = transforms.Compose([
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomVerticalFlip(),
+                transforms.Lambda(lambda x: x + 0.05 * torch.randn_like(x)),  # Add noise
+            ])
+
     def __len__(self):
         return len(self.states)
 
-    def __getitem__(self, i: int):
-        states = torch.from_numpy(self.states[i].copy()).float().to(self.device)
-        actions = torch.from_numpy(self.actions[i].copy()).float().to(self.device)
-        locations = (
-            torch.from_numpy(self.locations[i].copy()).float().to(self.device)
-            if self.locations is not None
-            else torch.empty(0).to(self.device)
-        )
+    def __getitem__(self, i):
+        states = torch.from_numpy(self.states[i].copy()).float()
+        actions = torch.from_numpy(self.actions[i]).float()
+
+        if self.augment:
+            augmented_states = []
+            for frame in states:
+                frame = self.augmentation_transforms(frame)
+                augmented_states.append(frame)
+            states = torch.stack(augmented_states)
+
+        states = states.to(self.device)
+        actions = actions.to(self.device)
+
+        if self.locations is not None:
+            locations = torch.from_numpy(self.locations[i].copy()).float().to(self.device)
+        else:
+            locations = torch.empty(0).to(self.device)
+
         return WallSample(states=states, locations=locations, actions=actions)
 
 
-def create_wall_dataloader(
-    data_path: str,
-    probing: bool = False,
-    device: str = "cuda",
-    batch_size: int = 64,
-    train: bool = True,
-):
-    ds = WallDataset(data_path=data_path, probing=probing, device=device)
-    return torch.utils.data.DataLoader(
+def create_wall_dataloader(data_path, probing=False, device="cuda", batch_size=64, train=True, augment=False):
+    ds = WallDataset(data_path=data_path, probing=probing, device=device, augment=augment)
+    loader = torch.utils.data.DataLoader(
         ds,
         batch_size=batch_size,
         shuffle=train,
         drop_last=True,
         pin_memory=False,
     )
+    return loader
