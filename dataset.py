41dc6da9045a890fb8b8207ca735a180c99d3ba3
Meteora623 <141472938+Meteora623@users.noreply.github.com>
2024-12-15 23:27:47 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ import numpy as np
 from typing import NamedTuple
 from torchvision import transforms
 from torch.utils.data import DataLoader
+import torchvision.transforms.functional as F
 
 class WallSample(NamedTuple):
     states: torch.Tensor
@@ -29,8 +30,9 @@ class WallDataset:
         self.device = device
 
         # 数据增强：随机裁剪和水平翻转
+        # 注：不再包含ToPILImage，因为我们手动将frame转为PIL
         self.transform = transforms.Compose([
-            transforms.RandomResizedCrop(size=65, scale=(0.8,1.0)),
+            transforms.RandomResizedCrop(size=65, scale=(0.8, 1.0)),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
         ])
@@ -39,25 +41,31 @@ class WallDataset:
         return self.states.shape[0]
 
     def __getitem__(self, i):
-        states = self.states[i]  # 原始为只读的mmap数组
-        states_copy = states.copy()  # 创建可写副本
+        # states: [T, C, H, W]的只读mmap数组
+        states = self.states[i] 
+        # 创建可写副本
+        states_copy = states.copy()  
         states_tensor = torch.from_numpy(states_copy).float()  # [T, C, H, W]
 
         # 对每帧进行数据增强
+        # 先将每帧转为PIL图，再通过transform增强
         for t in range(states_tensor.shape[0]):
             frame = states_tensor[t]  # [C, H, W]
-            frame_img = transforms.functional.to_pil_image(frame)
+            frame_img = F.to_pil_image(frame)  # 转为PIL图像
             frame_aug = self.transform(frame_img)  # 数据增强
             states_tensor[t] = frame_aug
 
-        actions = torch.from_numpy(self.actions[i]).float()
+        # 同理对 actions 和 locations 执行 copy 
+        actions_copy = self.actions[i].copy()
+        actions = torch.from_numpy(actions_copy).float()
+
         if self.locations is not None:
-            locations = torch.from_numpy(self.locations[i]).float()
+            locations_copy = self.locations[i].copy()
+            locations = torch.from_numpy(locations_copy).float()
         else:
             locations = torch.empty(0)
 
         return WallSample(states=states_tensor, locations=locations, actions=actions)
-
 
 def create_wall_dataloader(data_path, probing=False, device="cuda", batch_size=64, train=True):
     ds = WallDataset(data_path=data_path, probing=probing, device=device)
