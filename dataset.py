8b5bd7f581a85f9d140dd24a43a228b543ac3996
Meteora623 <141472938+Meteora623@users.noreply.github.com>
2024-12-15 23:09:03 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 from typing import NamedTuple
+from torchvision import transforms
+from torch.utils.data import DataLoader
 
 class WallSample(NamedTuple):
     states: torch.Tensor
@@ -8,12 +10,7 @@ class WallSample(NamedTuple):
     actions: torch.Tensor
 
 class WallDataset:
-    def __init__(
-        self,
-        data_path,
-        probing=False,
-        device="cuda",
-    ):
+    def __init__(self, data_path, probing=False, device="cuda"):
         print("Start loading states.npy with mmap_mode='r'...")
         self.states = np.load(f"{data_path}/states.npy", mmap_mode='r')
         print("states.npy loaded with shape:", self.states.shape)
@@ -31,39 +28,38 @@ class WallDataset:
 
         self.device = device
 
+        # 数据增强：随机裁剪和水平翻转
+        self.transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.RandomResizedCrop(size=65, scale=(0.8,1.0)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ])
+
     def __len__(self):
         return self.states.shape[0]
 
     def __getitem__(self, i):
-        states = torch.from_numpy(np.array(self.states[i])).float()
-        actions = torch.from_numpy(np.array(self.actions[i])).float()
+        states = self.states[i]  # np array [T, C, H, W]
+        # 转换为tensor后对每帧进行增广
+        # states shape: (T, 2, 65, 65)
+        states_tensor = torch.empty_like(torch.from_numpy(states), dtype=torch.float)
+        for t in range(states.shape[0]):
+            frame = torch.from_numpy(states[t]).float() # [2, 65, 65]
+            # 转换到PIL后需要C,H,W->H,W,C
+            frame_img = transforms.functional.to_pil_image(frame)
+            frame_aug = self.transform(frame_img) # [C,H,W]
+            states_tensor[t] = frame_aug
+
+        actions = torch.from_numpy(self.actions[i]).float()
         if self.locations is not None:
-            locations = torch.from_numpy(np.array(self.locations[i])).float()
+            locations = torch.from_numpy(self.locations[i]).float()
         else:
             locations = torch.empty(0)
-        return WallSample(states=states, locations=locations, actions=actions)
 
+        return WallSample(states=states_tensor, locations=locations, actions=actions)
 
-def create_wall_dataloader(
-    data_path,
-    probing=False,
-    device="cuda",
-    batch_size=64,
-    train=True,
-):
-    ds = WallDataset(
-        data_path=data_path,
-        probing=probing,
-        device=device,
-    )
-
-    loader = torch.utils.data.DataLoader(
-        ds,
-        batch_size=batch_size,
-        shuffle=train,
-        drop_last=True,
-        pin_memory=True,
-        num_workers=8,
-    )
-
+def create_wall_dataloader(data_path, probing=False, device="cuda", batch_size=64, train=True):
+    ds = WallDataset(data_path=data_path, probing=probing, device=device)
+    loader = DataLoader(ds, batch_size=batch_size, shuffle=train, drop_last=True, pin_memory=True, num_workers=4)
     return loader
